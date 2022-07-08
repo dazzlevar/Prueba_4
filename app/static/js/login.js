1eba87ba4d6d10772bcb9c244fe70a9d27bf806b
Victor <victor_aponte_bravo@hotmail.com>
2022-07-08 09:53:47 -0400

    fdfd

---
+++ app/static/js/login.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#id_username').css({"font-size": "1.6rem"});
+    $('#id_password').css({"font-size": "1.6rem"});
+    $('#login label').css({"font-size": "1.5rem"});
+});
---
+++ app/static/js/register.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#id_username').css({"font-size": "1.6rem"});
+    $('#id_first_name').css({"font-size": "1.6rem"});
+    $('#id_last_name').css({"font-size": "1.6rem"});
+    $('#id_email').css({"font-size": "1.6rem"});
+    $('#id_password1').css({"font-size": "1.6rem", "text-align": "center"});
+    $('#id_password2').css({"font-size": "1.6rem", "text-align": "center"});
+    $('#registro').css({"font-size": "1.5rem"});
+});
